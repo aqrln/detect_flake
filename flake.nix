e8298b3e1eeaf708b6ec9e93c29ee5a27e3e66eb
Alexey Orlenko <alex@aqrln.net>
2023-11-24 00:50:38 +0100

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "Detect flaky tests and non-deterministic behaviour";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, crane }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rust = pkgs.rust-bin.stable.latest.default;
+        craneLib = (crane.mkLib pkgs).overrideToolchain rust;
+
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
+
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            rust
+            rust-analyzer
+          ];
+        };
+
+        packages.default = craneLib.buildPackage rec {
+          src = craneLib.cleanCargoSource ./.;
+          buildInputs = [ ];
+          cargoArtifacts = craneLib.buildDepsOnly {
+            inherit src buildInputs;
+          };
+        };
+      }
+    );
+}
+
